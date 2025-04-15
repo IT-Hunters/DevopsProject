10415fb9d1769e7e484d57a22518c997d844c7ef
assilhammami <assil.hammami@esprit.tn>
2025-04-15 20:41:54 +0100

    update controller

--- src/main/java/com/esprit/tic/twin/springproject/controllers/TacheRestController.java
+++ src/main/java/com/esprit/tic/twin/springproject/controllers/TacheRestController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/taches")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TacheRestController {
     ITacheService tacheService;
 
