ceca0f949ee9f1d8f738b1b51c2c04f72e728dbf
wassimm1 <wassim.trabelsi@esprit.tn>
2025-04-17 21:23:23 +0200

    update port

--- src/main/java/com/esprit/tic/twin/springproject/controllers/TacheRestController.java
+++ src/main/java/com/esprit/tic/twin/springproject/controllers/TacheRestController.java
@@ -12,37 +12,5 @@ import java.util.List;
 @RequestMapping("/taches")
 @CrossOrigin(origins = "http://localhost:4202")
 public class TacheRestController {
-    ITacheService tacheService;
 
-    @PostMapping
-    public Tache addTache(@RequestBody Tache tache) {
-        return tacheService.addTache(tache);
-    }
-
-    @PutMapping
-    public Tache updateTache(@RequestBody Tache tache) {
-        return tacheService.updateTache(tache);
-    }
-
-    @GetMapping("/{id}")
-    public Tache retrieveTache(@PathVariable Long id) {
-        return tacheService.retrieveTache(id);
-    }
-
-    @GetMapping
-    public List<Tache> retrieveAllTaches() {
-        return tacheService.retrieveAllTaches();
-    }
-
-    @DeleteMapping("/{id}")
-    public void removeTache(@PathVariable Long id) {
-        tacheService.removeTache(id);
-    }
-
-    @PostMapping("/add-tasks-affect-etudiant/{nomEt}/{prenomEt}")
-    public List<Tache> addTasksAndAffectToEtudiant(@RequestBody List<Tache> tasks,
-                                                   @PathVariable("nomEt") String nomEt,
-                                                   @PathVariable("prenomEt") String prenomEt) {
-        return tacheService.addTasksAndAffectToEtudiant(tasks, nomEt, prenomEt);
-    }
 }
