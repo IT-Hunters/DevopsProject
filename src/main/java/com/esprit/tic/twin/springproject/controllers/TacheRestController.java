0385122a693bb6969498376ddc87bdb3c20cd514
wassimm1 <wassim.trabelsi@esprit.tn>
2025-04-17 21:30:04 +0200

    update port

--- src/main/java/com/esprit/tic/twin/springproject/controllers/TacheRestController.java
+++ src/main/java/com/esprit/tic/twin/springproject/controllers/TacheRestController.java
@@ -1,11 +1,10 @@
 package com.esprit.tic.twin.springproject.controllers;
 
-import com.esprit.tic.twin.springproject.entities.Tache;
-import com.esprit.tic.twin.springproject.services.ITacheService;
+
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+
 
 @RestController
 @AllArgsConstructor
--- src/main/java/com/esprit/tic/twin/springproject/services/TacheServiceImpl.java
+++ src/main/java/com/esprit/tic/twin/springproject/services/TacheServiceImpl.java
@@ -1,17 +1,12 @@
 package com.esprit.tic.twin.springproject.services;
 
 
-import com.esprit.tic.twin.springproject.entities.Tache;
 
-import com.esprit.tic.twin.springproject.repositories.EtudiantRepository;
-import com.esprit.tic.twin.springproject.repositories.TacheRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.util.List;
-import java.util.Set;
+
 @Slf4j
 @Service
 @AllArgsConstructor
