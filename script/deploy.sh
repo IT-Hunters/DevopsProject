ffd9812ea5bdb27c5d92d8d077e63c7f45839b16
bourgui123 <elyess.borji@esprit.tn>
2025-04-18 01:31:23 +0100

    update.

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 docker pull borji1234/elyess_borji_4twin4:latest
-docker-compose build app
-docker-compose up app mysql
+docker compose build app
+docker compose up app mysql
+
+#!/bin/bash
 
