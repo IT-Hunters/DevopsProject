d6244e648ac496b62a9afa9e2bf908f0cf60cc9a
wassimm1 <wassim.trabelsi@esprit.tn>
2025-04-18 06:22:46 +0200

    update pomxml  11

--- src/test/java/com/esprit/tic/twin/springproject/DEvopsProjectApplicationTests.java
+++ src/test/java/com/esprit/tic/twin/springproject/DEvopsProjectApplicationTests.java
@@ -27,7 +27,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-
+@SpringBootTest
 class DEvopsProjectApplicationTests {
 
 	@Mock
